2c06d7ce8fe3d69833532a48fffb1b53c4edca7d
justin-chen <justin.uob@gmail.com>
2017-08-23 16:05:39 +0800

    3 Types of AlertDialog

--- app/src/main/java/tw/com/nec/justin_chen/messageboxdemo/MainActivity.java
+++ app/src/main/java/tw/com/nec/justin_chen/messageboxdemo/MainActivity.java
@@ -1,9 +1,13 @@
 package tw.com.nec.justin_chen.messageboxdemo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -42,24 +46,74 @@ public class MainActivity extends AppCompatActivity {
         normalDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // normalDialogEvent();
-                Toast.makeText(MainActivity.this, "1 normalDialog", Toast.LENGTH_SHORT).show();
+                normalDialogEvent();
             }
         });
         listDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // listDialog();
-                Toast.makeText(MainActivity.this, "2 listDialog", Toast.LENGTH_SHORT).show();
+                listDialog();
             }
         });
         customDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // customDialog();
-                Toast.makeText(MainActivity.this, "3 customDialog", Toast.LENGTH_SHORT).show();
+                customDialog();
+//                Toast.makeText(MainActivity.this, "3 customDialog", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void normalDialogEvent(){
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(R.string.lunch_time)
+                .setMessage(R.string.want_to_eat)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.gogo, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.wait_a_minute, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.i_am_hungry, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton(R.string.not_hungry, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.diet, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+
+    private void listDialog(){
+        new AlertDialog.Builder(MainActivity.this)
+                .setItems(lunch.toArray(new String[lunch.size()]), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = lunch.get(which);
+                        Toast.makeText(getApplicationContext(), getString(R.string.u_eat) + name, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+
+    private void customDialog(){
+        final View item = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_layout, null);
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(R.string.input_ur_name)
+                .setView(item)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText editText = (EditText) item.findViewById(R.id.edit_text);
+                        Toast.makeText(getApplicationContext(), getString(R.string.hi) + editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
     }
 
 }
